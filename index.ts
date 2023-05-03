5ec2d653a875b920512c4b9c8deabb26e168b28f
Alexander Ruberto <a.ruberto@easygo.io>
2023-05-03 23:10:02 +1000

    first commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const port = 3000;
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+    <html>
+      <head>
+        <title>Node Benchmark</title>
+      </head>
+      <body>
+        <main>
+          <h1>Node Benchmark</h1>
+          <p>Current time: ${new Date().toISOString()}</p>
+          <p>Request to render time (ms): <span id='render-time' /></p>
+          <script>
+            const renderTime = new Date() - performance.timing.requestStart
+            document.getElementById('render-time').innerText = renderTime.toString()
+            console.log('Render time (ms):', renderTime)
+          </script>
+        </main>
+      </body>
+    </html>
+    `);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
